c8ba7c735be56531e5cccde22a4bf59f640d3ead
Thomas Lefebvre <thomaslfb3@gmail.com>
2025-02-13 15:51:50 +0100

    CHORE: first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+import Fastify from "fastify";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const fastify = Fastify({ logger: true });
+
+fastify.get("/", async (request, reply) => {
+  return { message: "Hello Fastify!" };
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: process.env.PORT || 3000, host: "0.0.0.0" });
+    console.log(`🚀 Server running at http://localhost:${process.env.PORT || 3000}`);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
