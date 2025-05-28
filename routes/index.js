8d8343dc5e20933d5d3c4f4c35eab0dab890dae8
Thomas Lefebvre <thomaslfb3@gmail.com>
2025-05-28 11:10:20 +0200

    FIX remove favorite routes

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,10 @@
 import imageRoutes from "./imagesRoutes.js";
 import authRoutes from "./authRoutes.js";
 import userRoutes from "./userRoutes.js";
-import favoriteRoutes from "./favoriteRoutes.js";
 import collectionRoutes from "./collectionRoutes.js";
 export default async function routes(fastify, options) {
   fastify.register(authRoutes);
   fastify.register(imageRoutes);
   fastify.register(userRoutes);
-  fastify.register(favoriteRoutes);
   fastify.register(collectionRoutes);
 }
