aebe0ebda3b64109d0aac8c4a9038c9c1d7d5535
Thomas Lefebvre <thomaslfb3@gmail.com>
2025-07-02 14:18:57 +0200

    FIX user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,18 @@
-import { getUserById, getAllUsers, updateUser, deleteUser } from "../models/user.js";
+import {
+  getUserById,
+  getAllUsers,
+  updateUser,
+  deleteUser,
+} from "../models/user.js";
 import { userSchema } from "../schemas/userSchema.js";
-import { userItemSchema,userItemUpdateSchema } from "../schemas/userItem.js";
-import { getItemsByUserId, addItemToUser, addItemToFavorites,removeItemFromFavorites, getLastItemByUserId} from "../models/item.js";
+import { userItemSchema, userItemUpdateSchema } from "../schemas/userItem.js";
+import {
+  getItemsByUserId,
+  addItemToUser,
+  addItemToFavorites,
+  removeItemFromFavorites,
+  getLastItemByUserId,
+} from "../models/item.js";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -37,14 +48,28 @@ export default async function userRoutes(fastify, options) {
   });
 
   fastify.get("/users/:id/lastItem", async (request, reply) => {
-    const item = await getLastItemByUserId(request.params.id);
-    reply.send(item || { message: "No items found for this user" });
+    const userItem = await getLastItemByUserId(request.params.id);
+
+    if (!userItem) {
+      return reply.send({ message: "No items found for this user" });
+    }
+
+    // Combine userItem and item data
+    const item = {
+      ...userItem.item,
+      is_favorite: userItem.is_favorite, // 👈 on ajoute ce champ
+    };
+
+    reply.send({ item });
   });
 
   fastify.post("/users/:id/addItem", async (request, reply) => {
     try {
       const validatedData = userItemSchema.parse(request.body);
-      const item = await addItemToUser(request.params.id, validatedData.item_id);
+      const item = await addItemToUser(
+        request.params.id,
+        validatedData.item_id
+      );
       reply.send(item);
     } catch (error) {
       reply.status(400).send({ error: error.errors });
@@ -54,7 +79,10 @@ export default async function userRoutes(fastify, options) {
   fastify.post("/users/:id/addFavorite", async (request, reply) => {
     try {
       const validatedData = userItemUpdateSchema.parse(request.body);
-      const item = await addItemToFavorites(request.params.id, validatedData.item_id);
+      const item = await addItemToFavorites(
+        request.params.id,
+        validatedData.item_id
+      );
       reply.send(item);
     } catch (error) {
       reply.status(400).send({ error: error.errors });
@@ -64,10 +92,18 @@ export default async function userRoutes(fastify, options) {
   fastify.post("/users/:id/removeFavorite", async (request, reply) => {
     try {
       const validatedData = userItemUpdateSchema.parse(request.body);
-      const item = await removeItemFromFavorites(request.params.id, validatedData.item_id);
+      const item = await removeItemFromFavorites(
+        request.params.id,
+        validatedData.item_id
+      );
       reply.send(item);
     } catch (error) {
       reply.status(400).send({ error: error.errors });
     }
-  } );
+  });
+
+  fastify.get("/users/:id/favorites", async (request, reply) => {
+    const favorites = await getItemsByUserId(request.params.id);
+    reply.send(favorites);
+  });
 }
--- services/gcsUploader.js
+++ services/gcsUploader.js
@@ -1,27 +1,31 @@
-import {Storage} from '@google-cloud/storage';
-import path from 'path';
-import fs from 'fs';
-import dotenv from 'dotenv';
+import { Storage } from "@google-cloud/storage";
+import path from "path";
+import fs from "fs";
+import dotenv from "dotenv";
 dotenv.config();
 
-
-const storage = new Storage({keyFilename:process.env.GOOGLE_APPLICATION_CREDENTIALS});
-const bucketName = "etiket_logs";
+const storage = new Storage({
+  keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+});
+const bucketName = "etiket_logs_storage";
 
 export const uploadLogsToGCS = async () => {
-    const date = new Date()
-    const dateStr = date.toISOString().split("T")[0];
-    const timeStr = date.toISOString().split("T")[1].replace(/:/g, "-").split(".")[0];
+  const date = new Date();
+  const dateStr = date.toISOString().split("T")[0];
+  const timeStr = date
+    .toISOString()
+    .split("T")[1]
+    .replace(/:/g, "-")
+    .split(".")[0];
 
-    const logFilePath = path.join("logs", `logs-${dateStr}.jsonl`);
-    const destination = `logs/${dateStr}/logs-${timeStr}.jsonl`;
+  const logFilePath = path.join("logs", `logs-${dateStr}.jsonl`);
+  const destination = `logs/${dateStr}/logs-${timeStr}.jsonl`;
 
-  
-    try {
-      await storage.bucket(bucketName).upload(logFilePath, { destination });
-      console.log(`✅ Log file uploaded to GCS: ${destination}`);
-      fs.unlinkSync(logFilePath); 
-    } catch (error) {
-      console.error("❌ Erreur upload GCS:", error);
-    }
-  };
+  try {
+    await storage.bucket(bucketName).upload(logFilePath, { destination });
+    console.log(`✅ Log file uploaded to GCS: ${destination}`);
+    fs.unlinkSync(logFilePath);
+  } catch (error) {
+    console.error("❌ Erreur upload GCS:", error);
+  }
+};
